671f1a3b3b45b8e707daf3c14f40a56f1f8f97dd
Abdullah0505 <>
2021-05-10 15:12:46 +0000

    edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Variable temporarily stores user's inputted age 
+// Variable temporarily stores user's inputted age
 let age = 0
 
 // Connect "Find Out" button to "findout" function
@@ -9,14 +9,14 @@ function findOut () {
   age = document.getElementById('answer').value
   age = parseInt(age)
 
-// Program determines what kind of movie user should watch
- if (age >= 17) {
+  // Program determines what kind of movie user should watch
+  if (age >= 17) {
     document.getElementById('text').innerHTML = 'You may view an r-rated movie'
-} else if (age >= 13) {
+  } else if (age >= 13) {
     document.getElementById('text').innerHTML = 'You may view a pg-13 movie'
-} else if (age >= 5) {
+  } else if (age >= 5) {
     document.getElementById('text').innerHTML = 'You may view a G-rated or PG movie'
-}  else {
-    document.getElementById('text').innerHTML = "Sorry, your too young to view most movies"
-}
+  } else {
+    document.getElementById('text').innerHTML = 'Sorry, your too young to view most movies'
+  }
 }
