4dca2e00b006140811b618c858c2b7379004d1c5
Abdullah0505 <>
2021-05-10 17:06:41 +0000

    spell check

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Variable temporarily stores user's inputted age
 let age = 0
 
-// Connect "Find Out" button to "findout" function
+// Connect "Find Out" button to "findOut" function
 document.getElementById('button').addEventListener('click', findOut)
 
 function findOut () {
@@ -11,9 +11,9 @@ function findOut () {
 
   // Program determines what kind of movie user should watch
   if (age >= 17) {
-    document.getElementById('text').innerHTML = 'You may view an r-rated movie'
+    document.getElementById('text').innerHTML = 'You may view an R-rated movie'
   } else if (age >= 13) {
-    document.getElementById('text').innerHTML = 'You may view a pg-13 movie'
+    document.getElementById('text').innerHTML = 'You may view a PG-13 movie'
   } else if (age >= 5) {
     document.getElementById('text').innerHTML = 'You may view a G-rated or PG movie'
   } else {
